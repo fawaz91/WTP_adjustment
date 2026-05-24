6e45dcd67af33c3fc687cb74a6ac23a3920d2538
fawaz91 <61297467+fawaz91@users.noreply.github.com>
2026-05-24 15:38:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.express as go
 
 st.set_page_config(
     page_title="HTA Price–Uncertainty Tool",
